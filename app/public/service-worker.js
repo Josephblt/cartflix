b357c6f7807d3feef62a9efc8d7eb17f49e5aa56
Wagner Scholl Lemos <josephblt@gmail.com>
2026-07-04 01:46:00 -0300

    Activate service worker immediately

--- app/public/service-worker.js
+++ app/public/service-worker.js
@@ -24,13 +24,14 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(
       keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-    ))
+    )).then(() => clients.claim())
   );
 });
 
